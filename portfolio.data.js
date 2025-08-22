2d30a8810675d0df7448b9dc1a060d11c2c0bb17
Grant Kozeny <grantk019@gmail.com>
2025-08-22 08:58:28 -0500

    Update description

--- portfolio.data.js
+++ portfolio.data.js
@@ -49,7 +49,9 @@ var skills = {
 var projects = [
   {
     name: 'Prototype Deep (Web) Research',
-    descriptions: [`Designed and implemented a deep (web) research app that surfaces highly relevant and personalized information based on the users specified interest.`],
+    descriptions: [`Designed and implemented a deep (web) research app that surfaces highly relevant and ` + 
+      `personalized information based on the users specified interest: Structured Output, Chain of Thought Prompting, ` +
+      `Workflow Design Patterns, Tools, Input Guardrails, Prompt Engineering, MCP Server.`],
     skills: [ 46, 47 ]
   },
   {
